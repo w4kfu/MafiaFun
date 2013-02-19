be58160f96edb4e843cf399f7693f8057869bb99
Samuel CHEVET <gw4kfu@gmail.com>
2013-02-19 17:47:39 +0100

    fix some style

--- dta_extractor/dta_extractor.c
+++ dta_extractor/dta_extractor.c
@@ -31,17 +31,17 @@ void Decypher(DWORD *Buffer, DWORD SizeBuffer, DWORD dwKey1, DWORD dwKey2)
     unsigned char *pBuf;
 
     pBuf = (unsigned char*)Buffer;
-	for (i = 0; i < SizeBuffer / 4; i += 2)
-	{
-		Buffer[i] = ~(~Buffer[i] ^ dwKey1);
-		Buffer[i + 1] = ~(~Buffer[i + 1] ^ dwKey2);
-		pBuf += 8;
-	}
-	for (i = 0; i < SizeBuffer % 8; i++)
-	{
-	    *pBuf = ~(~(*pBuf) ^ *pkey++);
-	    pBuf++;
-	}
+    for (i = 0; i < SizeBuffer / 4; i += 2)
+    {
+        Buffer[i] = ~(~Buffer[i] ^ dwKey1);
+        Buffer[i + 1] = ~(~Buffer[i + 1] ^ dwKey2);
+        pBuf += 8;
+    }
+    for (i = 0; i < SizeBuffer % 8; i++)
+    {
+        *pBuf = ~(~(*pBuf) ^ *pkey++);
+        pBuf++;
+    }
 }
 
 void HeaderInfo(struct file *sFile, struct dtaFile *Infodta)
@@ -198,7 +198,7 @@ int main(int argc, char *argv[])
 
 void hex_dump(void *data, int size)
 {
-	unsigned char *p = (unsigned char*)data;
+    unsigned char *p = (unsigned char*)data;
     unsigned char c;
     int n;
     char bytestr[4] = {0};
@@ -207,39 +207,36 @@ void hex_dump(void *data, int size)
     char charstr[16 * 1 + 5] = {0};
 
     for(n = 1; n <= size; n++)
-	{
+    {
         if (n % 16 == 1)
-		{
+        {
             sprintf_s(addrstr, sizeof(addrstr), "%.4x",
-               ((unsigned int)p-(unsigned int)data) );
+                ((unsigned int)p-(unsigned int)data));
         }
         c = *p;
         if (isalnum(c) == 0)
-		{
+        {
             c = '.';
         }
         sprintf_s(bytestr, sizeof(bytestr), "%02X ", *p);
         strncat(hexstr, bytestr, sizeof(hexstr)-strlen(hexstr)-1);
-
         sprintf_s(bytestr, sizeof(bytestr), "%c", c);
         strncat(charstr, bytestr, sizeof(charstr)-strlen(charstr)-1);
-
         if (n % 16 == 0)
-		{
+        {
             printf("[%4.4s]   %-50.50s  %s\n", addrstr, hexstr, charstr);
             hexstr[0] = 0;
             charstr[0] = 0;
         }
-		else if (n % 8 == 0)
-		{
+        else if (n % 8 == 0)
+        {
             strncat(hexstr, "  ", sizeof(hexstr)-strlen(hexstr)-1);
             strncat(charstr, " ", sizeof(charstr)-strlen(charstr)-1);
         }
         p++;
     }
-
     if (strlen(hexstr) > 0)
-	{
+    {
         printf("[%4.4s]   %-50.50s  %s\n", addrstr, hexstr, charstr);
     }
 }
