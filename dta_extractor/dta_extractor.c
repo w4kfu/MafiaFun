59b61052929fd083ff22931bb156aa42803055de
Samuel CHEVET <gw4kfu@gmail.com>
2013-02-19 17:40:05 +0100

    Fix bug

--- dta_extractor/dta_extractor.c
+++ dta_extractor/dta_extractor.c
@@ -1,53 +1,5 @@
 #include "dta_extractor.h"
 
-void hex_dump(void *data, int size)
-{
-	unsigned char *p = (unsigned char*)data;
-    unsigned char c;
-    int n;
-    char bytestr[4] = {0};
-    char addrstr[10] = {0};
-    char hexstr[16 * 3 + 5] = {0};
-    char charstr[16 * 1 + 5] = {0};
-
-    for(n = 1; n <= size; n++)
-	{
-        if (n % 16 == 1)
-		{
-            sprintf_s(addrstr, sizeof(addrstr), "%.4x",
-               ((unsigned int)p-(unsigned int)data) );
-        }
-        c = *p;
-        if (isalnum(c) == 0)
-		{
-            c = '.';
-        }
-        sprintf_s(bytestr, sizeof(bytestr), "%02X ", *p);
-        strncat(hexstr, bytestr, sizeof(hexstr)-strlen(hexstr)-1);
-
-        sprintf_s(bytestr, sizeof(bytestr), "%c", c);
-        strncat(charstr, bytestr, sizeof(charstr)-strlen(charstr)-1);
-
-        if (n % 16 == 0)
-		{
-            printf("[%4.4s]   %-50.50s  %s\n", addrstr, hexstr, charstr);
-            hexstr[0] = 0;
-            charstr[0] = 0;
-        }
-		else if (n % 8 == 0)
-		{
-            strncat(hexstr, "  ", sizeof(hexstr)-strlen(hexstr)-1);
-            strncat(charstr, " ", sizeof(charstr)-strlen(charstr)-1);
-        }
-        p++;
-    }
-
-    if (strlen(hexstr) > 0)
-	{
-        printf("[%4.4s]   %-50.50s  %s\n", addrstr, hexstr, charstr);
-    }
-}
-
 struct dtaFile dtaFiles[12] =
 {
     {"a2.dta", 0x1417d340, 0xb6399e19},
@@ -118,12 +70,12 @@ void FileEntryInfo(struct file *sFile, struct FileEntry *entry, struct dtaFile *
     BYTE lulz[0x8000];
     DWORD i;
 
-    printf("Unknow00 : %X\n", entry->Unknow00);
+    printf("TotalSize : %X\n", entry->TotalSize);
     printf("Unknow04 : %X\n", entry->Unknow04);
     printf("Unknow08 : %X\n", entry->Unknow08);
     printf("Unknow0C : %X\n", entry->Unknow0C);
     printf("FileSize : %X\n", entry->FileSize);
-    printf("Unknow14 : %X\n", entry->Unknow14);
+    printf("NbBlock : %X\n", entry->NbBlock);
     printf("NameLength : %X\n", entry->NameLength & 0x7FFF);
     printf("Unknow1C : %X\n", entry->Unknow1C);
 
@@ -242,4 +194,52 @@ int main(int argc, char *argv[])
     HeaderInfo(&sFile, Infodta);
     clean_file(&sFile);
     return 0;
+}
+
+void hex_dump(void *data, int size)
+{
+	unsigned char *p = (unsigned char*)data;
+    unsigned char c;
+    int n;
+    char bytestr[4] = {0};
+    char addrstr[10] = {0};
+    char hexstr[16 * 3 + 5] = {0};
+    char charstr[16 * 1 + 5] = {0};
+
+    for(n = 1; n <= size; n++)
+	{
+        if (n % 16 == 1)
+		{
+            sprintf_s(addrstr, sizeof(addrstr), "%.4x",
+               ((unsigned int)p-(unsigned int)data) );
+        }
+        c = *p;
+        if (isalnum(c) == 0)
+		{
+            c = '.';
+        }
+        sprintf_s(bytestr, sizeof(bytestr), "%02X ", *p);
+        strncat(hexstr, bytestr, sizeof(hexstr)-strlen(hexstr)-1);
+
+        sprintf_s(bytestr, sizeof(bytestr), "%c", c);
+        strncat(charstr, bytestr, sizeof(charstr)-strlen(charstr)-1);
+
+        if (n % 16 == 0)
+		{
+            printf("[%4.4s]   %-50.50s  %s\n", addrstr, hexstr, charstr);
+            hexstr[0] = 0;
+            charstr[0] = 0;
+        }
+		else if (n % 8 == 0)
+		{
+            strncat(hexstr, "  ", sizeof(hexstr)-strlen(hexstr)-1);
+            strncat(charstr, " ", sizeof(charstr)-strlen(charstr)-1);
+        }
+        p++;
+    }
+
+    if (strlen(hexstr) > 0)
+	{
+        printf("[%4.4s]   %-50.50s  %s\n", addrstr, hexstr, charstr);
+    }
 }
--- dta_extractor/dta_extractor.h
+++ dta_extractor/dta_extractor.h
@@ -28,20 +28,23 @@ struct dtaFile
 
 struct FileEntry
 {
-    DWORD Unknow00;
+    DWORD TotalSize; // Look like TotalSize and Block
     DWORD Unknow04;
     DWORD Unknow08;
     DWORD Unknow0C;
     DWORD FileSize;
-    DWORD Unknow14;
+    DWORD NbBlock;
     DWORD NameLength;
     DWORD Unknow1C;
 };
 
 BOOL check_signature(struct file *sFile);
-void HeaderInfo(struct file *sFile, struct dtaFile *Infodta);
 void Decypher(DWORD *Buffer, DWORD SizeBuffer, DWORD dwKey1, DWORD dwKey2);
-void TableInfo(struct file *sFile, struct dtaHeader *header, struct dtaFile *Infodta);
+void HeaderInfo(struct file *sFile, struct dtaFile *Infodta);
 void FileEntryInfo(struct file *sFile, struct FileEntry *entry, struct dtaFile *Infodta, DWORD dwOffset);
+void TableEntryInfo(struct TableEntry *entry);
+void TableInfo(struct file *sFile, struct dtaHeader *header, struct dtaFile *Infodta);
+struct dtaFile* GetInfodtaFile(LPCTSTR lpFileName);
+void hex_dump(void *data, int size);
 
 #endif // __DTA_EXTRACTOR_H__
